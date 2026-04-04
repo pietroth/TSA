dc908a4bb996b832145f81562ea5c9c06f477050
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-04 19:13:18 -0300

    Rename ConnectionPump (it had the wrong name) to ClientLifeCycleManager.

--- app/src/main/java/br/com/pietroth/tsa/core/network/protocol/ClientLifeCycleManager.java
+++ app/src/main/java/br/com/pietroth/tsa/core/network/protocol/ClientLifeCycleManager.java
@@ -3,7 +3,7 @@ package br.com.pietroth.tsa.core.network.protocol;
 import br.com.pietroth.tsa.core.network.transport.ConnectionCreatedListener;
 import br.com.pietroth.tsa.core.network.transport.Connection;
 
-public class ConnectionPump implements ConnectionCreatedListener {
+public class ClientManager implements ConnectionCreatedListener {
     
     @Override
     public void onConnectionCreated(Connection connection) {
