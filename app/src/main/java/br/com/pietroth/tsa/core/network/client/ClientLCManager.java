4430442f19847885fd96fe581064de0c730ec5d4
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-04 19:16:13 -0300

    Rename ClientLifeCycleManager to ClientLCManager

--- app/src/main/java/br/com/pietroth/tsa/core/network/client/ClientLCManager.java
+++ app/src/main/java/br/com/pietroth/tsa/core/network/client/ClientLCManager.java
@@ -1,9 +1,9 @@
-package br.com.pietroth.tsa.core.network.protocol;
+package br.com.pietroth.tsa.core.network.client;
 
 import br.com.pietroth.tsa.core.network.transport.ConnectionCreatedListener;
 import br.com.pietroth.tsa.core.network.transport.Connection;
 
-public class ClientManager implements ConnectionCreatedListener {
+public class ClientLCManager implements ConnectionCreatedListener {
     
     @Override
     public void onConnectionCreated(Connection connection) {
