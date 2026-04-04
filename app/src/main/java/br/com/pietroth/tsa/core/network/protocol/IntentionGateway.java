09ba58869a002d50b49d4e2224113c392ef2afd9
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-04 20:57:43 -0300

    Create IntentionGateway

--- app/src/main/java/br/com/pietroth/tsa/core/network/protocol/IntentionGateway.java
+++ app/src/main/java/br/com/pietroth/tsa/core/network/protocol/IntentionGateway.java
@@ -3,10 +3,10 @@ package br.com.pietroth.tsa.core.network.protocol;
 import br.com.pietroth.tsa.core.network.transport.Connection;
 import br.com.pietroth.tsa.core.network.transport.ConnectionReceivedListener;
 
-public class EventGateway implements ConnectionReceivedListener {
+public class IntentionGateway implements ConnectionReceivedListener {
 
     @Override
     public void onConnectionReceived(Connection connection, byte[] data) {
-        // Handle the received data
+        
     }
 }
