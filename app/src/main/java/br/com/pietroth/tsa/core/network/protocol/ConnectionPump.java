fa03456d79f254a7069273ac917fe715515bc5c4
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-04 18:57:49 -0300

    Create ConnectionPump in network.protocol

---
+++ app/src/main/java/br/com/pietroth/tsa/core/network/protocol/ConnectionPump.java
@@ -0,0 +1,12 @@
+package br.com.pietroth.tsa.core.network.protocol;
+
+import br.com.pietroth.tsa.core.network.transport.ConnectionCreatedListener;
+import br.com.pietroth.tsa.core.network.transport.Connection;
+
+public class ConnectionPump implements ConnectionCreatedListener {
+    
+    @Override
+    public void onConnectionCreated(Connection connection) {
+        
+    }
+}
