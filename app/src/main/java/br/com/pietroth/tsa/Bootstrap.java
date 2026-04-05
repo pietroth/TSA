90ec5c6a6f208209505aa760ef1d5bec33c197c9
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-05 18:31:53 -0300

    Add GameLoop dependecies in Bootstrap

--- app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
+++ app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
@@ -1,14 +1,42 @@
 package br.com.pietroth.tsa;
 
-public class Bootstrap {
-    public Bootstrap(Builder builder) {
+import br.com.pietroth.tsa.core.communication.event.codec.CodecRegistry;
+import br.com.pietroth.tsa.core.ecs.ECSRuntime;
 
+public class Bootstrap {
+    private final ECSRuntime ecsRuntime;
+    private final CodecRegistry registry;
+
+    public Bootstrap(Builder builder) {
+        this.ecsRuntime = builder.ecsRuntime;
+        this.registry = builder.registry;
+    }
+
+    public ECSRuntime getEcsRuntime() {
+        return ecsRuntime;
+    }
+
+    public CodecRegistry getCodecRegistry() {
+        return registry;
     }
 
     public static class Builder {
-        public Builder() {}
+        private ECSRuntime ecsRuntime;
+        private CodecRegistry registry;
+
+        public Builder ecsRuntime(ECSRuntime ecsRuntime) {
+            this.ecsRuntime = ecsRuntime;
+            return this;
+        }
+
+        public Builder codecRegistry(CodecRegistry registry) {
+            this.registry = registry;
+            return this;
+        }
 
         public Bootstrap build() {
+            if (ecsRuntime == null) throw new IllegalStateException("ECSRuntime is required");
+            if (registry == null) throw new IllegalStateException("CodecRegistry is required");
             return new Bootstrap(this);
         }
     }
