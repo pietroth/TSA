f8d6bab8c1a43140db256dd0be2cedeeda7ef2c3
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-18 21:13:46 -0300

    Use TargetScope in Event instead of int

--- app/src/main/java/br/com/pietroth/tsa/core/communication/event/Event.java
+++ app/src/main/java/br/com/pietroth/tsa/core/communication/event/Event.java
@@ -1,23 +1,24 @@
 package br.com.pietroth.tsa.core.communication.event;
 
 import br.com.pietroth.tsa.core.communication.MIDFData;
+import br.com.pietroth.tsa.core.communication.event.target.TargetScope;
 import br.com.pietroth.tsa.core.communication.MIDF;
 
 public class Event<T extends MIDFData> extends MIDF<T> {
     private final int originId;
-    private final int targetId;
+    private final TargetScope target;
 
-    public Event(byte family, byte type, T data, int originId, int targetId) {
+    public Event(byte family, byte type, T data, int originId, TargetScope target) {
         super(family, type, data);
         this.originId = originId;
-        this.targetId = targetId;
+        this.target = target;
     }
 
     public int getOriginId() {
         return originId;
     }
 
-    public int getTargetId() {
-        return targetId;
+    public TargetScope getTarget() {
+        return target;
     }
 }
