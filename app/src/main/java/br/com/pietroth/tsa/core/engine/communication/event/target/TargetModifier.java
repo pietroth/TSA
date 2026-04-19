04546260bdbd4933c1ad4d83830e92b67ccbf8bb
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-19 18:28:31 -0300

    Add engine.communication.event.target.TargetModifier.toList()

--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/target/TargetModifier.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/target/TargetModifier.java
@@ -1,4 +1,9 @@
 package br.com.pietroth.tsa.core.engine.communication.event.target;
 
+import java.util.List;
+
+import br.com.pietroth.tsa.core.engine.network.client.ClientId;
+
 public interface TargetModifier {
+    List<ClientId> toList();
 }
