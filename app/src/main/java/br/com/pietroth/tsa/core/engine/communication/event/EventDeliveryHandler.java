e25a367cf441d479c3a20491c6d297c9edecd183
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-19 18:40:36 -0300

    Add engine.communication.event.EventPublisher.publish()

---
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventDeliveryHandler.java
@@ -0,0 +1,7 @@
+package br.com.pietroth.tsa.core.engine.communication.event;
+
+import br.com.pietroth.tsa.core.engine.communication.MIDFData;
+
+public class EventDeliveryHandler {
+    public void delivery(Event<? extends MIDFData> event) {}
+}
--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventPublisher.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventPublisher.java
@@ -1,5 +1,16 @@
 package br.com.pietroth.tsa.core.engine.communication.event;
 
-public class EventBus {
-    
+import br.com.pietroth.tsa.core.engine.communication.MIDFData;
+
+public class EventPublisher {
+    private final EventDeliveryHandler deliveryHandler;
+
+    public EventPublisher(EventDeliveryHandler deliveryHandler) {
+        this.deliveryHandler = deliveryHandler;
+    }
+
+    public void publish(Event<? extends MIDFData> event) {
+        EventDispatcherSingleton.get().enqueue(event);
+        deliveryHandler.delivery(event);
+    }
 }
