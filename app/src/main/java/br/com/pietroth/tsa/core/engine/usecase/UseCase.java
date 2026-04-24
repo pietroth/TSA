8af853e76e7c5b4f7d72975d37e9d0dd9504bf5a
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-24 18:33:36 -0300

    Transform UseCase.execute(Intention<T> intention) into execute(T data)

--- app/src/main/java/br/com/pietroth/tsa/core/engine/usecase/UseCase.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/usecase/UseCase.java
@@ -1,8 +1,7 @@
 package br.com.pietroth.tsa.core.engine.usecase;
 
 import br.com.pietroth.tsa.core.engine.communication.MIDFData;
-import br.com.pietroth.tsa.core.engine.communication.intention.Intention;
 
 public interface UseCase<T extends MIDFData> {
-    void execute(Intention<T> intention);
+    void execute(T data);
 }
