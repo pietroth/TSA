8b4ae45ed70eb60561ed5d85916b29e49fcd462d
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-24 19:04:00 -0300

    Add playerId to PlayerMoveData

--- app/src/main/java/br/com/pietroth/tsa/core/game/player/playermovement/PlayerMoveData.java
+++ app/src/main/java/br/com/pietroth/tsa/core/game/player/playermovement/PlayerMoveData.java
@@ -3,10 +3,12 @@ package br.com.pietroth.tsa.core.game.player.playermovement;
 import br.com.pietroth.tsa.core.engine.communication.MIDFData;
 
 public class PlayerMoveData implements MIDFData {
+    public final int playerId;
     public final float sx;
     public final float sy;
 
-    public PlayerMoveData(float sx, float sy) {
+    public PlayerMoveData(int playerId, float sx, float sy) {
+        this.playerId = playerId;
         this.sx = sx;
         this.sy = sy;
     }
