6851ce4d3c64c3a7caf8cdcac507bba6e0b6524d
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-24 19:20:37 -0300

    Add originId and use playerId in PlayerEvents.publish_PlayerMoved

--- app/src/main/java/br/com/pietroth/tsa/core/game/player/PlayerEvents.java
+++ app/src/main/java/br/com/pietroth/tsa/core/game/player/PlayerEvents.java
@@ -2,19 +2,21 @@ package br.com.pietroth.tsa.core.game.player;
 
 import br.com.pietroth.tsa.core.engine.communication.event.Event;
 import br.com.pietroth.tsa.core.engine.communication.event.EventPublisherSingleton;
+import br.com.pietroth.tsa.core.engine.communication.event.target.OneClient;
+import br.com.pietroth.tsa.core.engine.communication.event.target.TargetScope;
 import br.com.pietroth.tsa.core.game.communication.MIDFGlossary;
 import br.com.pietroth.tsa.core.game.player.playermovement.PlayerMoveData;
 
 public class PlayerEvents {
 
-    public static void publish_PlayerMoved(int playerId, float sx, float sy) {
+    public static void publish_PlayerMoved(int originId, int playerId, float sx, float sy) {
         PlayerMoveData data = new PlayerMoveData(playerId, sx, sy);
         Event<PlayerMoveData> event = new Event<PlayerMoveData>(
             MIDFGlossary.Player.getGlobalId(),
             MIDFGlossary.Player.PLAYER_MOVED.getId(),
             data,
             1,
-            null
+            new TargetScope(new OneClient(playerId))
         );
         EventPublisherSingleton.get().publish(event);
     }
