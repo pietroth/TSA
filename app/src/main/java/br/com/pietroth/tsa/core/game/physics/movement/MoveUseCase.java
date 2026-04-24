620b6459439b6fc68c4dba0e86784fca5f577353
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-24 18:37:11 -0300

    Fix MoveUseCase

--- app/src/main/java/br/com/pietroth/tsa/core/game/physics/movement/MoveUseCase.java
+++ app/src/main/java/br/com/pietroth/tsa/core/game/physics/movement/MoveUseCase.java
@@ -1,21 +1,22 @@
-package br.com.pietroth.tsa.core.game.application;
+package br.com.pietroth.tsa.core.game.physics.movement;
 
 import br.com.pietroth.tsa.core.engine.ecs.ECSContainer;
-import br.com.pietroth.tsa.core.game.ecs.component.PositionComponent;
-import br.com.pietroth.tsa.core.game.ecs.component.VelocityComponent;
+import br.com.pietroth.tsa.core.engine.usecase.UseCase;
+import br.com.pietroth.tsa.core.game.player.playermovement.PlayerMoveData;
 
-public class MoveUseCase {
+public class MoveUseCase implements UseCase<PlayerMoveData> {
     private final ECSContainer container;
 
     public MoveUseCase(ECSContainer container) {
         this.container = container;
     }
 
-    public void execute(float sx, float sy) {
+    @Override
+    public void execute(PlayerMoveData data) {
         container.forEachEntityWith(new Class[]{VelocityComponent.class, PositionComponent.class}, entity -> {
             VelocityComponent velocity = entity.get(VelocityComponent.class);
-            velocity.x += sx;
-            velocity.y += sy;
+            velocity.x += data.sx;
+            velocity.y += data.sy;
         });
     }
 }
