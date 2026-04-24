13b6d78693537787f09af5dc9c003ea6d3565b00
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-24 18:30:57 -0300

    Transform UseCase interface into UseCase<T extends MIDFData>

--- app/src/main/java/br/com/pietroth/tsa/core/engine/usecase/UseCase.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/usecase/UseCase.java
@@ -3,6 +3,6 @@ package br.com.pietroth.tsa.core.engine.usecase;
 import br.com.pietroth.tsa.core.engine.communication.MIDFData;
 import br.com.pietroth.tsa.core.engine.communication.intention.Intention;
 
-public interface UseCase {
-    void execute(Intention<? extends MIDFData> intention);
+public interface UseCase<T extends MIDFData> {
+    void execute(Intention<T> intention);
 }
