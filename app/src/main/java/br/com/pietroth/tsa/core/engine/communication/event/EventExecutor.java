6f2c9fefe9016f60facf78a31e3ec1ce06186b0f
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-27 07:23:41 -0300

    Delete EventExecutors

--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventExecutor.java
+++
@@ -1,7 +0,0 @@
-package br.com.pietroth.tsa.core.engine.communication.event;
-
-import br.com.pietroth.tsa.core.engine.communication.MIDFData;
-
-public interface EventExecutor<T extends MIDFData> {
-    void execute(T event);
-}
---
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/runtime/ComponentResolver.java
@@ -0,0 +1,5 @@
+package br.com.pietroth.tsa.core.engine.runtime;
+
+public class ComponentResolver {
+    
+}
