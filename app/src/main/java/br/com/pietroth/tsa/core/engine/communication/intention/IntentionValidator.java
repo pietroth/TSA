cc8fb50511bdcb82d34fa32e5a7677716383144e
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-27 07:34:43 -0300

    Transform IntentionValidator into a FunctionalInterface

--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/intention/IntentionValidator.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/intention/IntentionValidator.java
@@ -2,6 +2,7 @@ package br.com.pietroth.tsa.core.engine.communication.intention;
 
 import br.com.pietroth.tsa.core.engine.communication.MIDFData;
 
+@FunctionalInterface
 public interface IntentionValidator<T extends MIDFData> {
     int validate(Intention<T> intention);
 }
