4c43e6e1534eb20e63d3efcd8f532fb3f5e06b2a
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-28 10:45:52 -0300

    Fix Bootstrap

--- app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
+++ app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
@@ -1,12 +1,11 @@
 package br.com.pietroth.tsa;
 
-import br.com.pietroth.tsa.core.engine.communication.event.EventDeliveryHandler;
 import br.com.pietroth.tsa.core.engine.communication.event.EventPublisherSingleton;
-import br.com.pietroth.tsa.core.engine.communication.MIDFEncoder;
 import br.com.pietroth.tsa.core.engine.communication.codec.CodecRegistry;
 import br.com.pietroth.tsa.core.engine.ecs.ECSRuntime;
 import br.com.pietroth.tsa.core.engine.network.client.ClientLCManagerSingleton;
 import br.com.pietroth.tsa.core.engine.network.protocol.IntentionGateway;
+import br.com.pietroth.tsa.core.engine.runtime.DataProcessingPipeline;
 import br.com.pietroth.tsa.core.engine.usecase.UseCaseRouter;
 import br.com.pietroth.tsa.core.game.Codecs;
 import br.com.pietroth.tsa.core.game.GameLoop;
@@ -26,12 +25,14 @@ public class Bootstrap {
     private final CodecRegistry codecRegistry;
     private final BlockRegister blockRegister;
     private final UseCaseRouter useCaseRouter;
+    private final DataProcessingPipeline dataProcessingPipeline;
 
     public Bootstrap(Builder builder) {
         this.ecsRuntime = builder.ecsRuntime;
         this.codecRegistry = builder.codecRegistry;
         this.blockRegister = builder.blockRegister;
         this.useCaseRouter = builder.useCaseRouter;
+        this.dataProcessingPipeline = builder.dataProcessingPipeline;
     }
 
     public void boot() {
@@ -46,7 +47,7 @@ public class Bootstrap {
             new PlayerMoveUseCase(ecsRuntime.getContainer())
         );
 
-        EventPublisherSingleton.init(new EventDeliveryHandler(new MIDFEncoder()));
+        EventPublisherSingleton.init(dataProcessingPipeline);
         IntentionVDSingleton.init();
 
         ClientLCManagerSingleton.init(
@@ -72,6 +73,7 @@ public class Bootstrap {
         private CodecRegistry codecRegistry;
         private BlockRegister blockRegister;
         private UseCaseRouter useCaseRouter;
+        private DataProcessingPipeline dataProcessingPipeline;
 
         public Builder ecsRuntime(ECSRuntime ecsRuntime) {
             this.ecsRuntime = ecsRuntime;
@@ -92,12 +94,18 @@ public class Bootstrap {
             this.useCaseRouter = useCaseRouter;
             return this;
         }
+        
+        public Builder dataProcessingPipeline(DataProcessingPipeline dataProcessingPipeline) {
+            this.dataProcessingPipeline = dataProcessingPipeline;
+            return this;
+        }
 
         public Bootstrap build() {
             if (ecsRuntime == null) throw new IllegalStateException("ECSRuntime is required");
             if (codecRegistry == null) throw new IllegalStateException("CodecRegistry is required");
             if (blockRegister == null) throw new IllegalStateException("BlockRegister is required");
             if (useCaseRouter == null) throw new IllegalStateException("UseCaseRouter is required");
+            if (dataProcessingPipeline == null) throw new IllegalStateException("DataProcessingPipeline is required");
             return new Bootstrap(this);
         }
     }
