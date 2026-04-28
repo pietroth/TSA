c445fb76ecff78e256e98aea1214b7686e172868
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-28 10:45:42 -0300

    Fix EventPublisherSingleton and Bootstrap

--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventPublisherSingleton.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/event/EventPublisherSingleton.java
@@ -1,13 +1,15 @@
 package br.com.pietroth.tsa.core.engine.communication.event;
 
+import br.com.pietroth.tsa.core.engine.runtime.DataProcessingPipeline;
+
 public class EventPublisherSingleton {
         private static EventPublisher instance;
 
-    public static void init(EventDeliveryHandler deliveryHandler) {
+    public static void init(DataProcessingPipeline processingPipeline) {
         if (instance != null) {
             throw new IllegalStateException("EventPublisherSingleton is already initialized");
         }
-        instance = new EventPublisher(deliveryHandler);
+        instance = new EventPublisher(processingPipeline);
     }
 
     public static EventPublisher get() {
