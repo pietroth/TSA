c2cc6415979b87f359e5710b8ab237a7409deaef
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-04-29 10:28:14 -0300

    Rename RIF by IR (Immediate Response)

--- app/src/main/java/br/com/pietroth/tsa/core/engine/communication/response/IR.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/response/IR.java
@@ -1,12 +1,12 @@
 package br.com.pietroth.tsa.core.engine.communication.response;
 
-// RIF means Rejected or Intention Filter /ˈrɪf/ (riff)
+// IR means Immediate Response /ˈaɪər/ (air)
 
-public class RIF {
+public class IR {
     private int correlationId; // Correlation ID with Intention and serves as a targetId
     private byte status;
 
-    public RIF(int status, int correlationId) {
+    public IR(int status, int correlationId) {
         if (status < 0 || status > 255) {
             throw new IllegalArgumentException("Status must be between 0 and 255");
         }
