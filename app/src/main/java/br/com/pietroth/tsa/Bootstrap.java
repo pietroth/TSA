efb394262eb9f45ceef13076d0574e14038e099b
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-02 16:11:16 -0300

    Init NetworkAggregatorSingleton in Bootstrap

--- app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
+++ app/src/main/java/br/com/pietroth/tsa/Bootstrap.java
@@ -2,6 +2,7 @@ package br.com.pietroth.tsa;
 
 import br.com.pietroth.tsa.core.engine.communication.event.EventPublisher;
 import br.com.pietroth.tsa.core.engine.communication.event.EventPublisherSingleton;
+
 import br.com.pietroth.tsa.core.engine.communication.MIDFEncoder;
 import br.com.pietroth.tsa.core.engine.communication.intention.IntentionDecoder;
 import br.com.pietroth.tsa.core.engine.communication.response.IRCodec;
@@ -9,6 +10,8 @@ import br.com.pietroth.tsa.core.engine.communication.response.IRPublisher;
 import br.com.pietroth.tsa.core.engine.communication.response.IRPublisherSingleton;
 import br.com.pietroth.tsa.core.engine.ecs.ECSRuntime;
 import br.com.pietroth.tsa.core.engine.network.MessageDeliveryHandler;
+import br.com.pietroth.tsa.core.engine.network.NetworkAggregator;
+import br.com.pietroth.tsa.core.engine.network.NetworkAggregatorSingleton;
 import br.com.pietroth.tsa.core.engine.network.client.ClientLCManager;
 import br.com.pietroth.tsa.core.engine.network.protocol.IntentionGateway;
 import br.com.pietroth.tsa.core.engine.runtime.ComponentResolver;
@@ -22,12 +25,14 @@ public class Bootstrap {
     private final BlockRegister blockRegister;
     private final ComponentResolver componentResolver;
     private final ClientLCManager clientLCManager;
+    private final NetworkAggregator networkAggregator;
 
     public Bootstrap(Builder builder) {
         this.ecsRuntime = builder.ecsRuntime;
         this.blockRegister = builder.blockRegister;
         this.componentResolver = builder.componentResolver;
         this.clientLCManager = builder.clientLCManager;
+        this.networkAggregator = builder.networkAggregator;
     }
 
     public void boot() {
@@ -35,6 +40,7 @@ public class Bootstrap {
 
         clientLCManager.setIntentionGateway(new IntentionGateway(componentResolver, new IntentionDecoder()));
 
+        NetworkAggregatorSingleton.init(networkAggregator);
         IRPublisherSingleton.init(new IRPublisher(new IRCodec(), deliveryHandler));
         EventPublisherSingleton.init(new EventPublisher(
             componentResolver,
@@ -59,6 +65,7 @@ public class Bootstrap {
         private BlockRegister blockRegister;
         private ComponentResolver componentResolver;
         private ClientLCManager clientLCManager;
+        private NetworkAggregator networkAggregator;
 
         public Builder ecsRuntime(ECSRuntime ecsRuntime) {
             this.ecsRuntime = ecsRuntime;
@@ -80,11 +87,17 @@ public class Bootstrap {
             return this;
         }
 
+        public Builder networkAggregator(NetworkAggregator networkAggregator) {
+            this.networkAggregator = networkAggregator;
+            return this;
+        }
+
         public Bootstrap build() {
             if (ecsRuntime == null) throw new IllegalStateException("ECSRuntime is required");
             if (blockRegister == null) throw new IllegalStateException("BlockRegister is required");
             if (componentResolver == null) throw new IllegalStateException("ComponentResolver is required");
             if (clientLCManager == null) throw new IllegalStateException("ClientLCManager is required");
+            if (networkAggregator == null) throw new IllegalStateException("NetworkAggregator is required");
             return new Bootstrap(this);
         }
     }
