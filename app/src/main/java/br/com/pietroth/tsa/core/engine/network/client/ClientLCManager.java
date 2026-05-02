a4af6a8de042decbd8a3403937394e015c556ac5
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-02 16:06:29 -0300

    Use fastutil's IntStack instead of AtomicInteger in ClientLCManager

--- app/src/main/java/br/com/pietroth/tsa/core/engine/network/client/ClientLCManager.java
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/network/client/ClientLCManager.java
@@ -5,22 +5,28 @@ import java.lang.foreign.MemorySegment;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import br.com.pietroth.tsa.core.engine.network.protocol.IntentionGateway;
 import br.com.pietroth.tsa.core.engine.network.transport.Connection;
 import br.com.pietroth.tsa.core.engine.network.transport.ConnectionCreatedListener;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntStack;
 
 public class ClientLCManager implements ConnectionCreatedListener {
     private final Client[] clients;
-    private final AtomicInteger idGenerator = new AtomicInteger(0);
+    private final IntStack freeIds; 
     private IntentionGateway intentionGateway;
     private final int maxClients;
 
     public ClientLCManager(int maxClients) {
         this.maxClients = maxClients;
         this.clients = new Client[maxClients];
+
+        this.freeIds = new IntArrayList(maxClients);
+        for (int i = maxClients - 1; i >= 0; i--) {
+            freeIds.push(i);
+        }
     }
 
     public void setIntentionGateway(IntentionGateway intentionGateway) {
@@ -29,11 +35,11 @@ public class ClientLCManager implements ConnectionCreatedListener {
 
     @Override
     public synchronized void onConnectionCreated(Connection connection) {
-        int id = idGenerator.getAndIncrement();
-        
-        if (id >= maxClients) {
-            throw new IllegalStateException("Maximum number of clients reached: " + maxClients);
+        if (freeIds.isEmpty()) {
+            throw new IllegalStateException("No available client IDs. Maximum number of clients reached: " + maxClients);
         }
+
+        int id = freeIds.popInt();
         
         connection.setId(id);
 
@@ -59,8 +65,9 @@ public class ClientLCManager implements ConnectionCreatedListener {
     }
 
     public void disconnectClient(int id) {
-        if (id >= 0 && id < maxClients) {
+        if (id >= 0 && id < maxClients && clients[id] != null) {
             clients[id] = null;
+            freeIds.push(id);
         }
     }
 
