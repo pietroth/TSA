1a18a9bea232b0211dc2ccc6bcba7d95446c46e1
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-03 17:20:30 -0300

    Finish PlayerLCManager.onConnectionProcessed()

--- app/src/main/java/br/com/pietroth/tsa/core/game/player/PlayerLCManager.java
+++ app/src/main/java/br/com/pietroth/tsa/core/game/player/PlayerLCManager.java
@@ -1,20 +1,32 @@
 package br.com.pietroth.tsa.core.game.player;
 
+import br.com.pietroth.tsa.core.engine.ecs.ECSContainer;
+import br.com.pietroth.tsa.core.engine.ecs.entity.ECSEntity;
 import br.com.pietroth.tsa.core.engine.network.protocol.ConnectionProcessedListener;
 import br.com.pietroth.tsa.core.engine.network.transport.Connection;
+import br.com.pietroth.tsa.core.game.physics.movement.PositionComponent;
+import br.com.pietroth.tsa.core.game.physics.movement.VelocityComponent;
 
 // LC = LifeCycle.
 
 public class PlayerLCManager implements ConnectionProcessedListener {
     private final Player2EntityResolver resolver;
+    private final ECSContainer container;
 
-    public PlayerLCManager(Player2EntityResolver resolver) {
+    public PlayerLCManager(Player2EntityResolver resolver, ECSContainer container) {
         this.resolver = resolver;
+        this.container = container;
     }
 
     @Override
     public void onConnectionProcessed(Connection connection) {
-        resolver.bind(connection.getId(), 0);
+        ECSEntity player = container.createEntity(
+            new PlayerComponent(connection.getId()),
+            new PositionComponent(0, 0),
+            new VelocityComponent(0, 0)
+        );
+
+        resolver.bind(connection.getId(), player.getId());
     }
     
 }
