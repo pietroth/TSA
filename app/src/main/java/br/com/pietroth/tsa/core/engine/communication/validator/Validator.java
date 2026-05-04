607f54fd78161f86bc9d61917c9b49835c4c8bbc
pietroth <pietrogabrielpelegrini2@gmail.com>
2026-05-04 14:17:01 -0300

    Move intention.Validator to validator.Validator

---
+++ app/src/main/java/br/com/pietroth/tsa/core/engine/communication/validator/Validator.java
@@ -0,0 +1,9 @@
+package br.com.pietroth.tsa.core.engine.communication.validator;
+
+import br.com.pietroth.tsa.core.engine.communication.MIDFData;
+import br.com.pietroth.tsa.core.engine.communication.intention.Intention;
+
+@FunctionalInterface
+public interface Validator<T extends MIDFData> {
+    int validate(Intention<T> intention);
+}
